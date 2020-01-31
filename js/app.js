7c34ed351285e6d787168be9c32b71df84ab16ad
Natasha Adler <natashaadler@Natashas-MBP.fc.ca>
2020-01-31 10:32:29 -0500

    Added image switching

--- js/app.js
+++ js/app.js
@@ -3,11 +3,22 @@
 
 	//buttons at the bottom of the page
 	const navButton = document.querySelectorAll("#buttonHolder img");
+				puzzleBoard = document.querySelector(".puzzle-board");
+				puzzlePiece = document.querySelectorAll(".puzzle-pieces img");
+
+				//store the image names here
+	const pieces = ["topLeft", "topRight", "bottomLeft", "bottomRight"];
 
 	function changeImageSet(){
-		//cange the thumbnail image s on the left to match the button images
+		//change the thumbnail images on the left to match the button images
+		//piece is a temperary variable & index is related to numbers
+		//make sure you index your images - makes it a lot easier to work with
+		pieces.forEach((piece, index) => puzzlePiece[index].src=`images/${piece + this.dataset.puzzleindex}.jpg`)
 		//set a background image on the drop xone container
-		debugger;
+		//style.backgroud to mutate the background
+		//`back tick` - pass through variable without having to declare them
+		//dataset.puzzleindex relates to line 34-37 in html
+		puzzleBoard.style.backgroundImage =`url(images/backGround${this.dataset.puzzleindex}.jpg)`
 	}
 
 	// add some event handling for the new navButtons
