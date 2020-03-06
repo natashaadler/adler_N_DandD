96108336d62f6cedc6fd6ea0d867df36b89e30e4
Natasha Adler <natashaadler@Natashas-MacBook-Pro.local>
2020-03-05 19:56:46 -0500

    Fixed the move puzzle to parent and finished the readme

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
+![PuzzlePicture](puzzleBanner.jpeg "Puzzle Banner")
+
 # Drag and Drop Example
 ## Natasha Adler
+
+This Assignment was based off of a simple puzzle game. While most features were working there were two significant ones that still needed to be fixed.
+
+<ul>
+  <li> Problem #1 – Multiple puzzle pieces can be dropped in one area and cannot be retrieved unless page is refreshed.</li>
+
+  <li>Problem #2 – When Switching between different puzzles(4), the pieces do not return to the side bar and “refresh”. </li>
+</ul>
+
+
+## Requirments
+
+Use a modern Browser such as [Firefox](https://www.mozilla.org/en-CA/firefox/new/) or [Chrome](https://www.google.ca/chrome/?brand=CHBD&gclsrc=aw.ds&&gclid=CjwKCAjw29vsBRAuEiwA9s-0B6zIdw5_qV4ETvbcN4042nlkfk9YggWT_DI1vM4UH4vWB2I0pdWUdhoCBWoQAvD_BwE). Avoid internet exploror. You will need git installed locally as well as a good code editor such as [Sublime](https://www.sublimetext.com) or [Atom](https://atom.io/).This site is built with HTML, CSS and JS.
+
+### Built With
+
+<ul>
+	<li>Html</li>
+	<li>CSS</li>
+	<li>JS</li>
+</ul>
+
+## My Solution
+
+Solution #1 – This had to do with the drop function. There was no limit on how many pieces could be dropped into each box on the puzzle-board. I made an if statement using length, stating that if there was >= 1 piece on the box, to then return it back to where the pieces are held (puzzle-pieces).  
+
+function allowDrop(event) {
+		if (this.children.length >= 1) {
+			return;
+		}
+
+Solution #2 –  This had to do with the puzzle pieces and moving them back into their parent div. I used append child to tell the puzzle pieces to move from its current position to the parent div once a new puzzle board had been clicked.
+
+let puzzlePieceBoard = document.querySelector(".puzzle-pieces");
+      puzzlePieceBoard.appendChild(puzzlePiece[index]);
+
+
+## Licence
+
+MIT
--- js/app.js
+++ js/app.js
@@ -16,8 +16,10 @@
 		//make sure you index your images - makes it a lot easier to work with
 		console.log( this.dataset.puzzleindex)
 		pieces.forEach((piece, index) => {
+			//moving puzzle pieces back to parent when clicking on a new puzzle
 			let puzzlePieceBoard = document.querySelector(".puzzle-pieces");
 			puzzlePieceBoard.appendChild(puzzlePiece[index]);
+			 //change thumbnail img
 			puzzlePiece[index].src=`images/${piece + this.dataset.puzzleindex}.jpg`;
 			puzzlePiece[index].id=`${piece + this.dataset.puzzleindex}`;
 		});
@@ -25,13 +27,12 @@
 
 		//set a background image on the drop xone container
 		//style.backgroud to mutate the background
-
 		//`back tick` - pass through variable without having to declare them
 		//dataset.puzzleindex relates to line 34-37 in html
 		puzzleBoard.style.backgroundImage =`url(images/backGround${this.dataset.puzzleindex}.jpg)`
 	}
 
-	//another loop - adding an event listener to the pulle piece
+	//another loop - adding an event listener to the pulled piece
 	function dragStart(event) {
 		console.log("started a drag");
 
@@ -43,6 +44,7 @@
 		event.preventDefault();
 		console.log("You dragged something to me");
 	}
+	//
 	function allowDrop(event) {
 		if (this.children.length >= 1) {
 			return;
@@ -54,15 +56,12 @@
 	}
 	// add some event handling for the new navButtons
 	navButton.forEach(button => button.addEventListener("click", changeImageSet))
-
 	//Set up drag event on the puzzle pieces
 	//drag start - when you start to drag
 	puzzlePiece.forEach(piece => piece.addEventListener("dragstart", dragStart));
+		//set up the drag over event for the drop zone
 	dropZone.forEach(zone => zone.addEventListener("dragover", allowDragOver));
-		dropZone.forEach(zone => zone.addEventListener("drop", allowDrop));
-	//set up the drag over event for the drop zone
-
-
+	dropZone.forEach(zone => zone.addEventListener("drop", allowDrop));
 
 	//like saying we have clicked on puzzle piece 0 (as it is the defult)
 	changeImageSet.call(navButton[0]);
